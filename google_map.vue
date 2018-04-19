2005ab4d0b638d148562fd2326ddd0414acc23fa
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-19 15:59:15 -0400

    South Oakville Centre 2.0  /google_map.vue

---
+++ google_map.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="" id="map"></div>
+</template>
+<style>
+    /* Always set the map height explicitly to define the size of the div
+       * element that contains the map. */
+      #map {
+        height: 100%;
+      }
+</style>
+
+<script>
+    define(["Vue", "vuex", 'google-map-api', 'axios'], function(Vue, Vuex, googleMapAPI, axios) {
+        return Vue.component("google-map", {
+            template: template, 
+            props: {
+                property: {
+                    type: Object,
+                    required: true
+                },
+                zoom: {
+                    type: Number,
+                    default: 15
+                }
+            },
+            data: function() {
+                return {
+                    position: null
+                }
+            },
+            mounted() {
+                // window.initMap();
+                console.log("hello", this.property);
+                property_string = this.property.address1+"+"+ this.property.city + "+" + this.property.country + "+" + this.property.postal_code;
+                console.log(property_string.replace(/ /g,"+"));
+                property_string = property_string.replace(/ /g,"+");
+                axios.get('https://maps.googleapis.com/maps/api/geocode/json?address='+property_string +'&key=AIzaSyCukCjH3fsuDYBdI44hZKL43m60jEToJjY').then(response => {
+                    // resolve(response);
+                    console.log("response", response.data.results[0]);
+                    geometry = response.data.results[0].geometry;
+                    var new_pos = {};
+                    new_pos.lat = geometry.location.lat;
+                    new_pos.lng = geometry.location.lng;
+                    this.position = new_pos;
+                    console.log(this.position)
+                });
+                // https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyCukCjH3fsuDYBdI44hZKL43m60jEToJjY
+                // this.initMap();
+            },
+            watch : {
+                position () {
+                    this.initMap();
+                }
+            },
+            methods : {
+                initMap() {
+            // Styles a map in night mode.
+            var map = new google.maps.Map(document.getElementById('map'), {
+              center: this.position,
+              zoom: this.zoom,
+              styles: googleMapTheme
+            });
+            var marker = new google.maps.Marker({
+              position: this.position,
+              map: map
+            });
+          }
+            }
+        });
+    });
+</script>
