606f462d788a45d359a114a82b7fe8c7ece34bc0
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-20 12:43:02 -0400

    South Oakville Centre 2.0 2.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -14,9 +14,9 @@
 			<div class="row padding_bottom_50 phone_padding_top_60">
                 <div class="col-md-12">
                     <div class="alpha_list">
-                        <a class="all_a" href="all">All</a>
+                        <a class="all_a" href="#all">All</a>
                         <a>#</a>
-                        <a v-for="letter in alphabet">{{letter}}</a>
+                        <a v-for="letter in alphabet"  href="#all">{{letter}}</a>
                     </div>
                 </div>
                 <div class="col-md-12">
