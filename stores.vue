b5774e410f4c06990fe35141878770a08ed08c9f
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-20 13:00:35 -0400

    South Oakville Centre 2.0 2.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -25,14 +25,14 @@
                             <a :name="key"></a>
                                 <span class="store_initial" :id="key" :data-initial="key">{{key}}</span>
                             </div>
-                            <div ></div>
+                            <div id="store_list_container" >
                             <div class="store_list" v-for="store in stores">
                                 <div class="store_list_content cats_row">
                                     <p class="store_name"><router-link :to="'/stores/'+store.slug">{{store.name}}</router-link></p>
                                 </div>
                             </div>   
                             </div>
-                            id="store_list_container" 
+                            </div>
                         </div>
                     </div>
                    
