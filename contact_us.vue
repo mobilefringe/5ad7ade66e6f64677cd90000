e4971bf5b54d85435fe3577a5cf90ca5f920fd5e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-20 14:10:40 -0400

    South Oakville Centre 2.0 2.0  /contact_us.vue

---
+++ contact_us.vue
@@ -0,0 +1,152 @@
+<template>
+    <div id="contact_us_container"> <!-- for some reason if you do not put an outer container div this component template will not render -->
+        <div  v-if="pageBanner" class="page_header" v-bind:style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
+			<div class="site_container">
+				<div class="header_content">
+					<h1>Contact Us</h1>
+				</div>
+			</div>
+		</div>  
+        <div class="margin_25_across padding_top_40 site_container">
+            <div class="row"> 
+                <div class="col-sm-6 text-left border_right" v-if="currentPage">
+            		<!--<div class="property_address">-->
+            		<!--     {{property.address1}} <br/> {{property.city}} <br/> {{property.province}} {{property.postal_code}} -->
+            		<!--</div>-->
+                    <div class="text-left" v-html="currentPage.body"></div>
+                </div> 
+                <div class="col-sm-6 contact_contents">
+                    <div class="description_text text-center caps contact_us_dets">
+                        CONTACT US FOR QUESTIONS, COMMENTS AND MORE INFORMATION.
+                    </div>
+                    <form class="form-horizontal padding_top_20" action="form-submit" @submit.prevent="validateBeforeSubmit">
+                        <div class="form-group ">
+                            <div class="col-sm-12" :class="{'has-error': errors.has('name')}">
+                                <label class="label" for="name">Name</label>
+                                <input v-model="form_data.name" v-validate="'required|alpha_spaces'" class="form-control" :class="{'input': true}" name="name" type="text" placeholder="Name" data-vv-delay="500">
+                                <span v-show="errors.has('name')" class="form-control-feedback">{{ errors.first('name') }}</span>
+                            </div>
+                            <div class="col-sm-12" :class="{'has-error': errors.has('email')}">
+                                <label class="label" for="email">Email</label>
+                                <input v-model="form_data.email" v-validate="'required|email'" class="form-control" :class="{'input': true}" name="email" type="email" placeholder="Email" data-vv-delay="500">
+                                <span v-show="errors.has('email')" class="form-control-feedback">{{ errors.first('email') }}</span>
+                            </div>
+                            <div class="col-xs-12" :class="{'has-error': errors.has('message')}">
+                                <label class="label" for="message">Message</label>
+                                <textarea v-model="form_data.message" v-validate="'required:true'" class="form-control" :class="{'input': true}" name="message" type="text" placeholder="Message" data-vv-delay="500"></textarea>
+                                <span v-show="errors.has('message')" class="form-control-feedback">{{ errors.first('message') }}</span>
+                            </div>
+                        </div>
+                        <div class="form-group account-btn text-left m-t-10">
+                            <div class="col-xs-12 text-center">
+                                <button class="contact_btn" type="submit" :disabled="formSuccess">Submit</button>
+                            </div>
+                        </div>
+                    </form>
+                    
+                    <div id="send_contact_success" class="alert alert-success text-left" role="alert" v-show="formSuccess">
+                        <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
+                        <span class="sr-only">Success</span>
+                        Thank you for contacting us. A member from our team will contact you shortly.
+                    </div>
+                    <div id="send_contact_error" class="alert alert-danger text-left" role="alert" v-show="formError">
+                        <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+                        <span class="sr-only">Error:</span>
+                        There was an error when trying to submit your request. Please try again later.
+                    </div>
+                    
+                </div>
+                
+            </div>
+            <div class="padding_top_40"></div>
+        </div>
+        <div style="height: 300px;margin-bottom:-30px;" class="show_phone">
+    	    <iframe class="grayscale" title="Map" width="100%" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"  src="http://maps.google.nl/maps?q=216 Chrislea RoadVaughanCanadaONON&hl=en&ie=UTF8&t=v&hnear=216 Chrislea RoadVaughanCanadaONON&z=16&output=embed">
+    			Map
+    		</iframe>
+		</div>
+    </div>
+</template>
+
+<script>
+    define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "vue-meta", 'vee-validate', 'utility'], function(Vue, Vuex, moment, tz, VueMoment, Meta, VeeValidate,Utility) {
+        Vue.use(Meta);
+        Vue.use(VeeValidate);
+        return Vue.component("contact-us-component", {
+            template: template, // the variable template will be injected
+            data: function() {
+                return {
+                    form_data : {},
+                    formSuccess : false,
+                    formError: false,
+                    currentPage : null,
+                    pageBanner: null
+                }
+            },
+            created(){
+                this.loadData().then(response => {
+                    this.currentPage = response[0].data;
+                    // var temp_repo = this.findRepoByName('Contact Us Banner');
+                    // if(temp_repo) {
+                    //     this.pageBanner = temp_repo.images[0];
+                    //     // this.pageBanner = {};
+                    //     // this.pageBanner.image_url = "//codecloud.cdn.speedyrails.net/sites/5ad8db786e6f64678e960000/image/png/1523030162566/contact_us_inside_banner.png"
+                    // }
+                     this.pageBanner = {};
+                        this.pageBanner.image_url = "//codecloud.cdn.speedyrails.net/sites/5ad8db786e6f64678e960000/image/png/1524238270214/stores_banner.png"
+                });
+            },
+            mounted () {
+                //ensuring the variables are created in this order for email
+                this.form_data.name = null;
+                this.form_data.email = null;
+                this.form_data.phone = null;
+                this.form_data.subject = this.property.name + ' Contact Us Form';
+                this.form_data.message = null;
+            },
+            computed: {
+                ...Vuex.mapGetters([
+                    'property',
+                    'timezone',
+                    'findRepoByName'
+                ]),
+            },
+            methods: {
+                validateBeforeSubmit() {
+                    this.$validator.validateAll().then((result) => {
+                        let errors = this.errors;
+                        send_data = {};
+                        send_data.form_data = JSON.stringify(Utility.serializeObject(this.form_data));
+                        this.$store.dispatch("CONTACT_US", send_data).then(res => {
+                            this.formSuccess = true;
+                        }).catch(error => {
+                            try {
+                                if (error.response.status == 401) {
+                                    console.log("Data load error: " + error.message);
+                                    this.formError = true;
+                                } 
+                                else {
+                                    console.log("Data load error: " + error.message);
+                                    this.formError = true;
+                                }
+                            } 
+                            catch (e) {
+                                console.log("Data load error: " + error.message);
+                                this.formError = true;
+                            }
+                        })
+                    })
+                },
+                loadData: async function() {
+                    try {
+                        // avoid making LOAD_META_DATA call for now as it will cause the entire Promise.all to fail since no meta data is set up.
+                        let results = await Promise.all([this.$store.dispatch('LOAD_PAGE_DATA', {url: this.property.mm_host + "	/pages/umber-contact-us.json"}),this.$store.dispatch("getData", "repos")]);
+                        return results;
+                    } catch (e) {
+                        console.log("Error loading data: " + e.message);
+                    }
+                },
+            }
+        });
+    });
+</script>
