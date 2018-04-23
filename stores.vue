ae1ac6c3163ec561ecbf03966b7a7333ab41ac37
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-23 10:02:29 -0400

    South Oakville Centre 2.0 2.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -27,7 +27,7 @@
                             <div id="store_list_container" >
                                 <div class="store_list" v-for="store in stores">
                                     <div class="store_list_content cats_row">
-                                        <p class="store_name">{{store.name}}</p>
+                                        <p class="store_name col-sm-6">{{store.name}}</p>
                                     </div>
                                 </div>   
                             </div>
