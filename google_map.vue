058636cdb364ebe944978e139eacdb41400fec64
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-23 17:29:41 -0400

    South Oakville Centre 2.0 2.0  /google_map.vue

--- google_map.vue
+++ google_map.vue
@@ -50,6 +50,7 @@
             },
             methods : {
                 initMap() {
+                    console.log("initMap");
                     // Styles a map in night mode.
                     var map = new google.maps.Map(document.getElementById('map'), {
                       center: this.position,
