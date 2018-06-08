ae9f241a01a7a823c1cf22756ccf4e5e524a85f1
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-06-08 16:14:44 -0400

    South Oakville Centre 2.0 2.0  /community_updates.vue

--- community_updates.vue
+++ community_updates.vue
@@ -100,7 +100,7 @@
     define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "vue-meta", 'vee-validate', 'utility', "vue!google_map"], function(Vue, Vuex, moment, tz, VueMoment, Meta, VeeValidate,Utility, GoogleMapAPI) {
         Vue.use(Meta);
         Vue.use(VeeValidate);
-        return Vue.component("about-us-component", {
+        return Vue.component("community-updates", {
             template: template, // the variable template will be injected
             data: function() {
                 return {
