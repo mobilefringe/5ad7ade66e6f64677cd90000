12cffe37e56297409d216af336752ddd77dea267
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-06-11 11:45:12 -0400

    South Oakville Centre 2.0 2.0  /routes.js

--- routes.js
+++ routes.js
@@ -20,7 +20,7 @@ define([], function() {
             ]
         },
         {
-            path: '/about_us',
+            path: '/contact',
             component: view('default'),
             children: [{
                 path: '',
